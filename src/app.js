293983fa2515a315fec34e24d399dc6710b47dc9
MrPotatoJr <90863275+MrPotatoJr@users.noreply.github.com>
2024-12-30 01:59:17 +0000

    Cardgenerator completed

--- src/app.js
+++ src/app.js
@@ -6,6 +6,30 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // Listas de números y símbolos
+  let numeros = ["A", 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K"];
+  let iconos = ["♥", "♦", "♣", "♠"];
+
+  // Función para obtener un índice aleatorio
+  function random(lista) {
+    return Math.floor(Math.random() * lista.length);
+  }
+
+  // Generar número y símbolo aleatorios
+  let numCard = numeros[random(numeros)];
+  let iconCard = iconos[random(iconos)];
+
+  document.getElementById("number").textContent = numCard;
+  document.getElementById("top-suit").textContent = iconCard;
+  document.getElementById("bottom-suit").textContent = iconCard;
+
+  if (iconCard === "♥" || iconCard === "♦") {
+    document.getElementById("top-suit").style.color = "red";
+    document.getElementById("bottom-suit").style.color = "red";
+    document.getElementById("number").style.color = "red";
+  } else {
+    document.getElementById("top-suit").style.color = "black";
+    document.getElementById("bottom-suit").style.color = "black";
+    document.getElementById("number").style.color = "black";
+  }
 };
